195fc6626714a593f9f129c033e333fd62bb204f
stefano <sirfoga@protonmail.com>
2019-07-15 16:31:37 +0200

    refactor EosConfiguration

--- hyperion/configs/configuration.py
+++ hyperion/configs/configuration.py
@@ -3,9 +3,8 @@
 
 """ Configuration module """
 
+import abc
 import json
-import ntpath
-import re
 
 from logs.logger import get_custom_logger
 
@@ -19,14 +18,14 @@ class Configuration:
     def categories(self):
         return self.get_matrioska_config(['meta', 'categories'])
 
-    def _parse(self):
-        self.logger.log_message('parse', self.config_file)
-        raise NotImplementedError
+    @abc.abstractmethod
+    def _parse(self, reader):
+        return {}
 
     def _get_config(self, key):
         if not self.data:  # cache
             with open(self.config_file) as reader:
-                self.data = json.load(reader)
+                self.data = self._parse(reader)
 
         return self.data[key]
 
@@ -46,51 +45,18 @@ class Configuration:
 
         return current_matrioska
 
+
+class JsonConfiguration(Configuration):
+    def _parse(self, reader):
+        return json.load(reader)
+
+
+class EosConfiguration(JsonConfiguration):
     def get_db_name(self):
         return self.get_matrioska_config(['db', 'name'])
 
-    def get_folder_of(self, key):
-        return self.get_matrioska_config([key, 'folder'])
+    def get_coll_name(self):
+        return self.get_matrioska_config(['db', 'coll'])
 
-    def get_file_regex_of(self, key):
-        return self.get_matrioska_config([key, 'file regex'])
-
-    def get_simulation_id_regex(self):
-        return self.get_matrioska_config(['simulation', 'id regex group'])
-
-    def get_file_id_regex(self):
-        return self.get_matrioska_config(['simulation', 'file id regex group'])
-
-    def get_collection_name_of(self, key):
-        return self.get_matrioska_config([key, 'collection name'])
-
-    def get_file_regex(self, file_name):
-        if file_name.startswith('Walker'):
-            return self.get_file_regex_of('Walker')
-        elif file_name.startswith('AveData'):
-            return self.get_file_regex_of('AveData')
-        elif file_name.startswith('Tau_e_'):
-            return self.get_file_regex_of('TauData')
-        elif file_name.endswith('_FLIPBOXES0_200_300Mpc_lighttravel'):
-            return self.get_file_regex_of('LightConeBoxes')
-        elif file_name.startswith('_200_300Mpc_LightConeSlice'):
-            return self.get_file_regex_of('LightConsSlices')
-
-        return None
-
-    def get_simulation_data(self, file_path):
-        file_name = ntpath.basename(file_path)
-
-        file_regex = self.get_file_regex(file_name)
-        if file_regex:
-            match = re.search(file_regex, file_name)
-            out = {
-                'id': match.group(self.get_simulation_id_regex())
-            }
-
-            if len(match.groups()) > 1:  # there are more groups
-                out['file id'] = match.group(self.get_file_id_regex())
-
-            return out
-
-        return None
+    def get_src_folder(self):
+        return self.get_matrioska_config(['system', 'folder'])
